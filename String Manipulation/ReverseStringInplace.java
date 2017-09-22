d89028f0c0d8427de9bdd57a911cae06932d5fe2
snehalphatangare <snehalphatangare@gmail.com>
2017-09-22 16:00:18 -0700

    Create ReverseStringInplace.java

---
+++ String Manipulation/ReverseStringInplace.java
@@ -0,0 +1,18 @@
+//Write a function that takes a string as input and returns the string reversed.
+//The below method is to reverse the string in place. TC=O(n/n), SP=O(1)
+class Solution {
+    public String reverseString(String s) {
+        int i=0,j=s.length()-1;
+        char[] reverseArr = new char[s.length()];
+        while(i<=j){
+           /* System.out.println("******s.charAt(i)"+s.charAt(i));
+            System.out.println("******s.charAt(j)"+s.charAt(j));*/
+            reverseArr[i]=s.charAt(j);
+            reverseArr[j]=s.charAt(i);
+            i++;
+            j--;
+            
+        }
+        return String.valueOf(reverseArr);
+    }
+}
