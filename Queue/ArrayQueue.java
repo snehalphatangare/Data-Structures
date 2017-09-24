fae2457fb3563c52a81e035b1f5aa5c333bf3471
snehalphatangare <snehalphatangare@gmail.com>
2017-09-24 13:01:58 -0700

    Update ArrayQueue.java

--- Queue/ArrayQueue.java
+++ Queue/ArrayQueue.java
@@ -2,57 +2,65 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class ArrayQueue {
-	static final int MAX = 1000;
+	static final int MAX = 2;
 	int front, rear, size;
     int array[] = new int[MAX];
       
     public ArrayQueue() {
-         front  = 0; 
-         rear = - 1;
+         front  = -1; 
+         rear = -1;
          array = new int[MAX];
            
     }
       
-    // Queue is full when size becomes equal to 
-    // the capacity 
+    // Queue is full when rear =size-1
     boolean isFull(ArrayQueue queue)
     {  return (rear == MAX-1);
     }
       
-    // Queue is empty when size is 0
+    // Queue is empty when front = rear =-1
     boolean isEmpty(ArrayQueue queue)
-    {  return (front > rear); }
+    {  return (front ==  -1 && rear ==-1); }
       
     // Method to add an item to the queue. 
     // 1. Check if queue is full 2. Increase rear. 3. Insert element at rear
     void enqueue( int item)
     {
         if (isFull(this)){
-        	System.out.println("Queue FUll");
+        	System.out.println("********Queue FUll*******");
             return ;
         }
-        array[++rear]=item;
+        else if(isEmpty(this))
+        	front=rear=0;
+        else
+        	rear++;
+        array[rear]=item;
         System.out.println(item+ " enqueued to queue");
     }
       
     // Method to remove an item from queue.  
-    // 1. Check is queue is empty 2.Get the element at front. 3. Decrement front
+    // 1. Check is queue is empty 2.Get the element at front. 3. Increment front
     Object dequeue()
     {
         if (isEmpty(this)){
-        	System.out.println("Queue Empty");
+        	System.out.println("*******Queue Empty*******");
         	return null;
     	}  
-        int item = this.array[this.front++];
+        int item=array[front];
+        if(front==rear)
+        	front=rear=-1;
+        else 
+        	front++;
         return item;
     }
       
     void print(){
-    	for(int i=front;i<=rear;i++){
-			System.out.print(array[i]+" -- ");
-		}
-    	
-    	System.out.println("null");
+    	if(front>=0){
+	    	for(int i=front;i<=rear;i++){
+				System.out.print(array[i]+" -- ");
+			}
+	    	System.out.println("null");
+    	}
     }
 
 	public static void main(String[] args) {
