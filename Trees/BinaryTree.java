8457ce2763c145884560d67bed8b2f3042ab5e05
snehalphatangare <snehalphatangare@gmail.com>
2017-09-30 19:13:37 -0700

    Updated to add postOrderTraversal methods

--- Trees/BinaryTree.java
+++ Trees/BinaryTree.java
@@ -17,6 +17,12 @@ class Node {
 		left=right=null;
 		data=val;
 	}
+	
+	public boolean isLeaf(){
+		if(this.left==null && this.right==null)
+			return true;
+		return false;
+	}
 }
 
 
@@ -39,14 +45,6 @@ public class BinaryTree {
 		}
 	}
 	
-	public void postOrderTraversal(Node root){
-		if(root!=null){
-			postOrderTraversal(root.left);
-			postOrderTraversal(root.right);
-			System.out.print(root.data + " ");
-		}
-	}
-	
 	public void inOrderTraversal(Node root){
 		if(root!=null){
 			inOrderTraversal(root.left);
@@ -55,6 +53,21 @@ public class BinaryTree {
 		}
 	}
 	
+	public void postOrderTraversal(Node root){
+		if(root!=null){
+			postOrderTraversal(root.left);
+			postOrderTraversal(root.right);
+			System.out.print(root.data + " ");
+		}
+	}
+	
+	//*****Recursive algos use the system stack. So for Non-recursive i.e iterative solutions use stack DS
+	/*Algo:
+	 1. Push root in stack
+	 2. Until stack is empty
+	 	Pop and print node.data
+	 	Push right child, then left (Bcz want to print left then right).
+	 */
 	public void preOrderTraversalNR(Node root){
 		Stack<Node> s= new Stack<Node>();
 		if(root==null){
@@ -74,6 +87,12 @@ public class BinaryTree {
 		
 	}
 	
+	/*Algo:
+	 1. Mark curr=root. Push root in stack
+	 2. Until done !=true
+	 	2.1 Push all left nodes of curr to stack until reach left leaf node. curr=curr.left
+	 	2.1 Now pop, print and push its right to stack. curr= poppedNode.right
+	 */
 	public void inOrderTraversalNR(Node root){
 		Stack<Node> s= new Stack<Node>();
 		if(root==null){
@@ -99,8 +118,40 @@ public class BinaryTree {
 					
 			}
 		}
-		
-		
+	}
+	
+	/*Algo:
+	 1. Push root in stack
+	 2.  Until stack is empty
+	 	2.1 Peek the stack
+	 		If leaf node- pop and print it
+	 		Else Push right child, then left (Bcz want to print left then right). Assign these pushed left & right nodes to null to make peeked node as leaf node. (bcz after printing 
+	 		left & right we want to print this node)
+	 */
+	public void postOrderTraversalNR(Node root){
+		Stack<Node> s= new Stack<Node>();
+		if(root==null){
+			System.out.println("Tree is empty");
+			return;
+		}
+		s.push(root);
+		while(!s.isEmpty()){
+			Node n=s.peek();
+			if(n.isLeaf()){
+				s.pop();
+				System.out.print(n.data+" ");
+			}
+			else{
+				if(n.right!=null){
+					s.push(n.right);
+					n.right=null;
+				}
+				if(n.left!=null){
+					s.push(n.left);
+					n.left=null;
+				}
+			}
+		}
 	}
 	
 	public static void main(String[] args) {
@@ -113,15 +164,20 @@ public class BinaryTree {
 		tree.root.right.left= new Node(6);
 		tree.root.right.right=new Node(7);
 		
-		System.out.println("Pre Order Traversal: ");
+		System.out.println("\n Pre Order Traversal: ");
 		tree.preOrderTraversal(tree.root);
 		System.out.println();
 		tree.preOrderTraversalNR(tree.root);
 		
-		System.out.println("In Order Traversal: ");
+		System.out.println("\n In Order Traversal: ");
 		tree.inOrderTraversal(tree.root);
 		System.out.println();
 		tree.inOrderTraversalNR(tree.root);
+		
+		System.out.println("\n Post Order Traversal: ");
+		tree.postOrderTraversal(tree.root);
+		System.out.println();
+		tree.postOrderTraversalNR(tree.root);
 	}
 
 }
