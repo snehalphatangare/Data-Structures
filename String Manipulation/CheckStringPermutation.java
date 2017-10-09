cec183c7cb23420ac5dcd1d72c72a77b2fd669b3
snehalphatangare <snehalphatangare@gmail.com>
2017-10-09 00:07:33 -0700

    Update CheckStringPermutation.java

--- String Manipulation/CheckStringPermutation.java
+++ String Manipulation/CheckStringPermutation.java
@@ -1,5 +1,5 @@
 /*
- * Check Whether Two Strings are Permutations of each other
+ * Check Whether Two Strings are Permutations of each other. Try not to use additional space
  */
 package Strings;
 import java.util.Arrays;
@@ -7,6 +7,13 @@ import java.util.Scanner;
 
 public class CheckStringPermutation {
 	
+	//Approach 1
+	//Iterate and compare every element with other to find 
+	//T.C=O(n^2) S.C=O(1)
+	
+	/*Approach 2
+	Sort both strings and compare
+	T.C=O(N LogN) S.C=O(1)*/
 	private static Boolean checkPermutation(String str1,String str2){
 		if(str1==str2)
 			return true;
@@ -15,15 +22,19 @@ public class CheckStringPermutation {
 		
 		char[] temp1 = str1.toCharArray();
 		Arrays.sort(temp1);
-		System.out.println("temp1="+temp1.toString());
-		str1 = temp1.toString();
+		String newStr1 = new String(temp1);
 		temp1 = str2.toCharArray();
 		Arrays.sort(temp1);
-		System.out.println("temp2="+temp1);
-		str2 = temp1.toString();
-		System.out.println("string 1 and 2 are"+str1+" "+str2);
-		return str1.equals(str2);
+		String newStr2 = new String(temp1);
+		System.out.println("string 1 and 2 are "+newStr1+" "+newStr2);
+		return newStr1.equals(newStr2);
 	}
+	
+	/*Approach 3
+	Use HashTable to store elements of longer array and its count
+	Navigate the smaller string. check if that char­ac­ter exist­ing in hash table, if yes then decrease its count by 1 and if it doesn’t exist then return false.
+	If for an element the count is already 0 before decrementing, then return false.
+	T.C=O(N) S.C=O(N)*/
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
