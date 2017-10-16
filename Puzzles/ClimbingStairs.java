ddb1391d9044abda84b57def47cb647d52d7f3d6
snehalphatangare <snehalphatangare@gmail.com>
2017-10-15 20:43:29 -0700

    Update ClimbingStairs.java

--- Puzzles/ClimbingStairs.java
+++ Puzzles/ClimbingStairs.java
@@ -9,21 +9,39 @@ public class ClimbingStairs {
 	     int numOfStairs = scan.nextInt();
 	     System.out.println("\n Enter number of steps a person can climb at a time ");
 	     int numOfSteps = scan.nextInt();
-	     int result=countWaysDP(numOfStairs,numOfSteps);
+	     int result=countWays(numOfStairs,numOfSteps);
 	     System.out.printf("Number of ways the person can climb %d stairs taking upto %d steps at time is %d", numOfStairs,numOfSteps,result);
+	     result=countWaysDP(numOfStairs,numOfSteps);
+	     System.out.printf("\nDP -->Number of ways the person can climb %d stairs taking upto %d steps at time is %d", numOfStairs,numOfSteps,result);
+	}
+	
+	public static int countWays(int numOfStairs,int numOfSteps){
+		int n =numOfStairs+1;
+		int noOfWays[] = new int[n];
+		if(n<=2)
+			return 1;
+		int secondLast=1,last=1;
+		int current=0;
+		for (int i=2; i<n; i++){
+			current=secondLast+last;
+			secondLast=last;
+			last=current;
+		}
+		return current;
 	}
 	
 	public static int countWaysDP(int numOfStairs,int numOfSteps){
 		int n =numOfStairs+1;
 		int noOfWays[] = new int[n];
-		noOfWays[0] = 1; noOfWays[1] = 1;
-        for (int i=2; i<n; i++)
-        {
-        	noOfWays[i] = 0;
-           for (int j=1; j<=numOfSteps && j<=i; j++)
-        	   noOfWays[i] += noOfWays[i-j];
-        }
-        return noOfWays[n-1];
+		noOfWays[0] = 1; 
+		noOfWays[1] = 1;
+		for (int i=2; i<n; i++)
+		{
+			noOfWays[i] = 0;
+		   for (int j=1; j<=numOfSteps && j<=i; j++)
+			   noOfWays[i] += noOfWays[i-j];
+		}
+        	return noOfWays[n-1];
 	}
 
 }
