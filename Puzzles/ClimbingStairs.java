6bb924aeb3791c9b9958944798f1f3fdeddb2a6c
snehalphatangare <snehalphatangare@gmail.com>
2017-10-15 20:32:28 -0700

    Create ClimbingStairs.java

---
+++ Puzzles/ClimbingStairs.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ClimbingStairs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		 System.out.println("\n Enter number of stairs ");
+	     int numOfStairs = scan.nextInt();
+	     System.out.println("\n Enter number of steps a person can climb at a time ");
+	     int numOfSteps = scan.nextInt();
+	     int result=countWaysDP(numOfStairs,numOfSteps);
+	     System.out.printf("Number of ways the person can climb %d stairs taking upto %d steps at time is %d", numOfStairs,numOfSteps,result);
+	}
+	
+	public static int countWaysDP(int numOfStairs,int numOfSteps){
+		int n =numOfStairs+1;
+		int noOfWays[] = new int[n];
+		noOfWays[0] = 1; noOfWays[1] = 1;
+        for (int i=2; i<n; i++)
+        {
+        	noOfWays[i] = 0;
+           for (int j=1; j<=numOfSteps && j<=i; j++)
+        	   noOfWays[i] += noOfWays[i-j];
+        }
+        return noOfWays[n-1];
+	}
+
+}
